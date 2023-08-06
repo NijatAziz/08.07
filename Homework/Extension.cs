658db5046b17b1624bb4acbbb0048d0454c41128
NijatAziz <nijatfaz@code.edu.az>
2023-08-06 15:38:06 +0400

    first commit

---
+++ Homework/Extension.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Homework
+{
+    internal static class Extension
+    {
+        public static bool CustomContains(this string mainText, string searchText)
+        {
+            string mainLower = mainText.ToLower();
+            string searchLower = searchText.ToLower();
+
+            int index = 0;
+            foreach (char c in mainLower)
+            {
+                if (c == searchLower[index])
+                {
+                    index++;
+                    if (index == searchLower.Length)
+                        return true;
+                }
+                else
+                {
+                    index = 0;
+                }
+            }
+
+            return false;
+        }
+
+
+
+        public static string[] CustamSplit(this string sentence,char symbol)
+        {
+            string symbl = " ";
+            string[] arr = new string[0];
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+             if (sentence[i]==symbol )
+                {
+                    Array.Resize(ref arr, arr.Length + 1);
+                    arr[arr.Length - 1] = symbl;
+                    symbl = string.Empty;
+                }
+                else
+                {
+                    symbl += sentence[i];
+                }
+            }
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = symbl;
+            return arr;
+        }
+    }
+
+
+}
---
+++ Homework/Program.cs
@@ -0,0 +1,22 @@
+﻿using Homework;
+
+internal class Program
+{
+    private static void Main()
+    {
+        //string mainText = "Bu bir custom contains methodudur";
+        //string searchText = "xx";
+        //bool contains = mainText.CustomContains(searchText);
+        //Console.WriteLine($"'{searchText}' cumlede ardicil olaraq varmi? {contains}");
+
+
+
+        string sentence = "Bu bir custom,split methodudur";
+        string[] words = sentence.CustamSplit('bir');
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
+
+    }
+}
